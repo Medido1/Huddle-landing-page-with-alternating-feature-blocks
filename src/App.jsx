8bfaa3d695f8d8ebb0b45e1522ba270da431b18d
Medido1 <medi1222@gmail.com>
2025-04-21 19:13:07 +0100

    Set up react + tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <p className='ml-50 text-5xl'>
+      Test react + tailwind
+    </p>
+  )
+}
+
+export default App
