17e4f2377f773486778c438ade10a4a585e5e9df
Medido1 <medi1222@gmail.com>
2025-05-25 17:52:00 +0100

    Finish hero section for mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,20 @@
 import bgHeroMobile from "../assets/images/bg-hero-mobile.svg";
 import bgHeroDesktop from "../assets/images/bg-hero-desktop.svg";
+import mockups from "../assets/images/illustration-mockups.svg"
 import logo from "../assets/images/logo.svg";
 import "../App";
  
 
 function Header() {
   return (
-    <header className="relative h-[500px] bg-[#E6FAFB]">
+    <header className="relative  bg-[#E6FAFB]">
       <img
         src={bgHeroMobile}
         className="absolute top-0 left-0 w-full h-full object-cover z-0"
         alt="Hero background"
       />
-      <div className="relative z-10 px-6 py-8">
-        <div className="flex justify-between items-center">
+      <div className="relative z-10 px-6 py-8 flex flex-col items-center">
+        <div className="w-full flex justify-between items-center">
           <img src={logo} alt="logo" className="w-24 h-6" />
           <button className="bg-white rounded-xl p-2 shadow-lg">Try it Free</button>
         </div>
@@ -25,9 +26,16 @@ function Header() {
           but so does your audience. 
           Create connections with your users as you engage in genuine discussion. 
         </p>
+        <button className="bg-[#FF4EDC] text-white font-bold px-12 py-3 rounded-full
+          mt-8 shadow-lg cursor-pointer">
+          Get Started For Free
+        </button>
+        <img 
+          className="mt-14"
+          src={mockups} 
+          alt="illustration mockups" />
       </div>
     </header>
-
   )
 }
 
