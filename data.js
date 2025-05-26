20857b27b1d1022de57552bcfa82e6929de07da8
Medido1 <medi1222@gmail.com>
2025-05-26 18:05:32 +0100

    Finsih main cards

--- data.js
+++ data.js
@@ -1,4 +1,6 @@
 import growTogether from "./src/assets/images/illustration-grow-together.svg";
+import flowingConversation from "./src/assets/images/illustration-flowing-conversation.svg";
+import yourUsers from "./src/assets/images/illustration-your-users.svg"
 
 const cards = [
   {
@@ -9,6 +11,22 @@ const cards = [
     description: `Generate meaningful discussions with your audience and build a strong, 
       loyal community.Think of the insightful conversations you miss 
       out on with a feedback form.`,
+  },
+  {
+    id:2,
+    img:flowingConversation,
+    altText: "Flowing conversation illustration",
+    title: "Flowing Conversations",
+    description: `You wouldn't paginate a conversation in real life, so why do it online? Our threads 
+      have just-in-time loading for a more natural flow.`
+  },
+  {
+    id:3,
+    img: yourUsers,
+    altText: "Your users illustration",
+    title: "Your Users",
+    description: `It takes no time at all to integrate Huddle with your app's authentication solution. 
+      This means, once signed in to your app, your users can start chatting immediately.`
   }
 ]
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 function Card({card}) {
   return (
-    <div className="bg-white mt-10 rounded-lg p-6">
+    <div className="bg-white mt-5 rounded-lg p-6">
         <img src={card.img} alt={card.altText} />
         <p className="font-poppins text-center text-xl mt-10 mb-5">
           {card.title}
